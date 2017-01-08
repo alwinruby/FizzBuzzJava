94ba85052aec3b0b709cbf61ab8ccc6b6898c005
alwinruby <alwinsolanky@gmail.com>
2017-01-08 17:52:13 +0000

    first commit

---
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+package com.fizzbuzz;
+
+public class FizzBuzz {
+
+	public static String getNumber(int i) {
+		if (i % 15 == 0) {
+			return "FizzBuzz";
+		} else if (i % 3 == 0) {
+			return "Fizz";
+		} else if (i % 5 == 0) {
+			return "Buzz";
+		} else {
+			return Integer.toString(i);
+		}
+	}
+}
